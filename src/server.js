c915f71f97851195fd826e6ab08aa1aabcc4b4c0
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-25 01:15:27 +0100

    lesson-1 in progress

--- src/server.js
+++ src/server.js
@@ -12,4 +12,22 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const log = (req, res, next) => {
+  console.log('logging')
+  next()
+}
+
+app.get('/data', log, (req, res) => {
+  res.send({ data: [1, 2, 3] })
+})
+
+app.post('/data', (req, res) => {
+  console.log(req.body)
+  res.send({ ok: true })
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
