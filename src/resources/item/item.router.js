81888d216403c27bfc566bea9edcecebb258cf65
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-25 03:55:19 +0100

    lesson-2 done

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,21 @@
 import { Router } from 'express'
-
 const router = Router()
+
+const mockController = (req, res) => {
+  res.json({ message: 'hello' })
+}
+
+// /api/item
+router
+  .route('/')
+  .get(mockController)
+  .post(mockController)
+
+// /api/item/:id
+router
+  .route('/:id')
+  .get(mockController)
+  .put(mockController)
+  .delete(mockController)
 
 export default router
