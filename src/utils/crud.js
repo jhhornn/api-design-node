1ec18101416560bcc7e9164e3bdba6efdd6b7dd8
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-26 16:58:21 +0100

    working on crud.js

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,8 +1,27 @@
-export const getOne = model => async (req, res) => {}
+import { models } from 'mongoose'
 
-export const getMany = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const userId = req.user._id
 
-export const createOne = model => async (req, res) => {}
+  const doc = await model.findOne({ _id: id, createdBy: userId }).exec()
+
+  if (!doc) {
+    return res.status(404).end()
+  }
+
+  res.status(200).json({ data: doc })
+}
+
+export const getMany = model => async (req, res) => {
+  const docs = await model.find({ createdBy: req.user._id })
+  res.status(200).json({ data: docs })
+}
+
+export const createOne = model => async (req, res) => {
+  const doc = await model.create({ ...req.body, createdBy: req.user._id })
+  res.status(200).json({ data: doc })
+}
 
 export const updateOne = model => async (req, res) => {}
 
