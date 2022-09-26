af21c87800ca79cc0bacc0b11998522daf524127
Awosise Oluwaseun <awosiseo@gmail.com>
2022-09-26 10:13:14 +0100

    item model done

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -14,8 +14,21 @@ const itemSchema = new mongoose.Schema(
       enum: ['active', 'complete', 'pastdue'],
       default: 'active'
     },
-    notes: String
+    notes: String,
+    due: Date,
+    createdBy: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+      ref: 'user'
+    },
+    list: {
+      type: mongoose.SchemaTypes.ObjectId,
+      required: true,
+      ref: 'list'
+    }
   },
   { timestamps: true }
 )
+
+itemSchema.index({ list: 1, name: 1 }, { unique: true })
 export const Item = mongoose.model('item', itemSchema)
